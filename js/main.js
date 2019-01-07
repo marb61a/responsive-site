83dfc8bf53d4e2bef0e444705a0db6559330f2e0
martin byrne <marb61a@gmail.com>
2019-01-07 16:33:58 +0000

    Added scroll and nav click functions to main .js

--- js/main.js
+++ js/main.js
@@ -17,10 +17,39 @@
         const pageWidth = window.innerWidth;
         const navContainer = document.querySelector("header nav .aw-container");
         const navigation = document.querySelector("header nav .aw-navigation");
+        const mobileNavigation = document.querySelector("body > .aw-navigation");
         
+        if(pageWidth <= mobileWidth && navigation) {
+            document.body.insertAdjacentElement("afterbegin", navigation);
+        } else if(pageWidth > mobileWidth && mobileNavigation) {
+            navContainer.insertAdjacentElement("beforeend", mobileNavigation);
+        }
     };
     
     const onNavItemClick = () => {
+        const navItemList = document.querySelectorAll(".aw-section-link");
+        const navItems = [...navItemList];
         
+        navItems.forEach(item => {
+            item.addEventListener("click", event => {
+                event.preventDefault();
+                const sectionId = event.target.getAttribute("href") || event.target.dataset.href;
+
+                scrollToSection(sectionId);
+            });
+        });
+    };
+    
+    const scrollToSection = sectionId => {
+        let sectionPosition, sectionOffset;
+        const navigationHeight = document.querySelector("header nav").offsetHeight;
+        const pageWidth = window.innerWidth;
+        
+        if(sectionId !== "#") {
+            sectionOffset = document.querySelector(sectionId).offsetTop;
+            sectionPosition = pageWidth > mobileWidth ? sectionOffset - navigationHeight : sectionOffset;
+        } else {
+            sectionPosition = 0;
+        }
     };
 })();
