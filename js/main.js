e7e5f862bf5b4f3341fd027777ce4914cb1aa1a8
martin byrne <marb61a@gmail.com>
2019-01-08 12:36:33 +0000

    added gallery click and image open functions to main.js

--- js/main.js
+++ js/main.js
@@ -76,4 +76,26 @@
         });
     };
     
+    const onGalleryImageClick = () => {
+        const galleryImageList = document.querySelectorAll("#aw-gallery li");
+        const galleryImages = [...galleryImageList];
+        
+        galleryImages.forEach(image => {
+            image.addEventListener("click", event => {
+                galleryImageOpen(event.target);
+            });
+        });
+    };
+    
+    const galleryImageOpen = image => {
+        const imageSrc = image.getAttribute("src");
+        const openedImage = `
+            <div class='aw-backdrop'>
+                <img src='${imageSrc}' alt='' />
+	            <span class="aw-backdrop-close">X</span>
+		    </div>`;
+
+		document.body.insertAdjacentHTML("beforeend", openedImage);
+    };
+    
 })();
